2a6b9dae233cf9429894ab31be5eb89026de2e8b
BerryGod <84345559+BerryGod@users.noreply.github.com>
2021-11-01 19:24:03 +0100

    Delete commands/Szia directory

--- commands/Szia/Szia.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "szia",
-    category: "Szia parancs",
-    description: "test command",
-    run: async(bot, message, args) => {
-        message.channel.send("Hello")
-    }
-}
