82f307a6f06b74617ef120711ac8d8f9e601c211
BerryGod <84345559+BerryGod@users.noreply.github.com>
2021-11-04 20:29:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -242,6 +242,26 @@ if(cmd === `${prefix}giveaway`){
     }
     
     
+    if (cmd === `${prefix}clear`) {
+        if (message.member.permissions.has('ADMINISTRATOR')) {
+            if (args[0] && isNaN(args[0]) && args[0] <= 100 || 0 < args[0] && args[0] < 101) {
+
+                let clearEmbed = new Discord.MessageEmbed()
+                    .setTitle(`Törölve lett ${Math.round(args[0])} Üzenet a chatből!`)
+                    .setColor("#ff1800")
+                    .setAuthor(message.author.username)
+                    .setTimestamp()
+
+                message.channel.send(clearEmbed);
+
+
+                message.channel.bulkDelete(Math.round(args[0]))
+
+
+            }
+        }
+    }
+    
 //////////////////////////////////////////////////////////
 })
 
