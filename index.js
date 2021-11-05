cbe14fd579a5cd94d1358f280034166f4dcd3081
BerryGod <84345559+BerryGod@users.noreply.github.com>
2021-11-05 16:30:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ const ms = require("ms");
 const os = require('os');
 
 
-const ytdl = require("ytdl-core")
-const queue = new Map()
 ///////////////////////////////////////////
 
 
@@ -383,106 +381,7 @@ if(cmd === `${prefix}giveaway`){
     
     
     
-    	const serverQueue = queue.get(message.guild.id);
-
-	    if (message.content.startsWith(`${prefix}play`)) {
-		    execute(message, serverQueue);
-		    return;
-	    } else if (message.content.startsWith(`${prefix}skip`)) {
-		    skip(message, serverQueue);
-		    return;
-	    } else if (message.content.startsWith(`${prefix}stop`)) {
-		    stop(message, serverQueue);
-		    return;
-	    } else {
-		    return;
-	    }
-    });
-
-    async function execute(message, serverQueue) {
-	    const args = message.content.split(' ');
-
-	    var voiceChannel = message.member.voiceChannel
-	    if(!message.member.voiceChannel){
-            message.channel.send("Bent kell legyél egy voice szobában!")
-            return;
-        }
-	    const permissions = voiceChannel.permissionsFor(message.client.user);
-	    if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
-		return message.channel.send('kő permission ._.');
-	    }
-    
-	    const songInfo = await ytdl.getInfo(args[1]);
-	    const song = {
-		    title: songInfo.title,
-		    url: songInfo.video_url,
-	    };
-
-	    if (!serverQueue) {
-		    const queueContruct = {
-			    textChannel: message.channel,
-			    voiceChannel: voiceChannel,
-			    connection: null,
-			    songs: [],
-			    volume: 5,
-			    playing: true,
-		    };
-
-		    queue.set(message.guild.id, queueContruct);
-
-		    queueContruct.songs.push(song);
-
-		    try {
-			    var connection = await voiceChannel.join();
-			    queueContruct.connection = connection;
-			    play(message.guild, queueContruct.songs[0]);
-		    } catch (err) {
-			    console.log(err);
-			    queue.delete(message.guild.id);
-			    return message.channel.send(err);
-		    }
-	    } else {
-		    serverQueue.songs.push(song);
-		    console.log(serverQueue.songs);
-		    return message.channel.send(`${song.title} várólistába kerűt!`);
-	    }
-
-    }
-
-    function skip(message, serverQueue) {
-	    if (!message.member.voiceChannel) return message.channel.send('voice szobában kell legyé!');
-	    if (!serverQueue) return message.channel.send('nem tok zenére skipelni!');
-	    serverQueue.connection.dispatcher.end();
-    }
-
-    function stop(message, serverQueue) {
-	    if (!message.member.voiceChannel) return message.channel.send('voice szobában ke legyé!');
-	    serverQueue.songs = [];
-	    serverQueue.connection.dispatcher.end();
-    }
-
-    function play(guild, song) {
-	    const serverQueue = queue.get(guild.id);
-
-	    if (!song) {
-		    serverQueue.voiceChannel.leave();
-		    queue.delete(guild.id);
-		    return;
-	    }
-
-	    const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
-		    .on('end', () => {
-			    serverQueue.songs.shift();
-			    play(guild, serverQueue.songs[0]);
-		    })
-		    .on('error', error => {
-			    console.error(error);
-		    });
-	    dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-    }
-    
-    
-    
+  
     
     
     
@@ -498,6 +397,6 @@ if(cmd === `${prefix}giveaway`){
     
     
 //////////////////////////////////////////////////////////
-
+})
 
 bot.login(process.env.BOT_TOKEN);
