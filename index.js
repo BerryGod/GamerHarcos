0ce1d0f652352657f9779273bef1a43589447694
BerryGod <84345559+BerryGod@users.noreply.github.com>
2021-11-05 17:18:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -385,19 +385,7 @@ if(cmd === `${prefix}giveaway`){
         
     
     
-     if (cmd === `${prefix}floppa`) {
-        const subreddits = ["Floppa"]
-        const random = subreddits[Math.floor(Math.random() * subreddits.length)]
 
-        const IMG = await randomPuppy(random)
-        const MemeEmbed = new Discord.MessageEmbed()
-            .setColor("RANDOM")
-            .setImage(IMG)
-            .setTitle(`Keresési szöveg: ${random} (KATT IDE!)`)
-            .setURL(`https://www.reddit.com/r/${random}`)
-
-        message.channel.send(MemeEmbed)
-    }
     
 
     
