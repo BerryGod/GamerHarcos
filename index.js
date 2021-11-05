c83c16220a609e6710006d74deed5d15ad9225c5
BerryGod <84345559+BerryGod@users.noreply.github.com>
2021-11-05 16:45:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ var maxrandom = 5; // 0-5 seconds added to movement interval (randomly)
 var host = data["ip"];
 var username = data["name"]
 var nightskip = data["auto-night-skip"]
-var bot = mineflayer.createBot({
+var bota = mineflayer.createBot({
   host: host,
   username: username
 });
@@ -408,44 +408,44 @@ if(cmd === `${prefix}giveaway`){
     
     
     
-        bot.on('time', function(time) {
+        bota.on('time', function(time) {
 	    if(nightskip == "true"){
-	    if(bot.time.timeOfDay >= 13000){
-	    bot.chat('/time set day')
+	    if(bota.time.timeOfDay >= 13000){
+	    bota.chat('/time set day')
 	    }}
         if (connected <1) {
             return;
         }
         if (lasttime<0) {
-            lasttime = bot.time.age;
+            lasttime = bota.time.age;
         } else {
             var randomadd = Math.random() * maxrandom * 20;
             var interval = moveinterval*20 + randomadd;
-            if (bot.time.age - lasttime > interval) {
+            if (bota.time.age - lasttime > interval) {
                 if (moving == 1) {
-                    bot.setControlState(lastaction,false);
+                    bota.setControlState(lastaction,false);
                     moving = 0;
                     lasttime = bot.time.age;
                 } else {
                     var yaw = Math.random()*pi - (0.5*pi);
                     var pitch = Math.random()*pi - (0.5*pi);
-                    bot.look(yaw,pitch,false);
+                    bota.look(yaw,pitch,false);
                     lastaction = actions[Math.floor(Math.random() * actions.length)];
-                    bot.setControlState(lastaction,true);
+                    bota.setControlState(lastaction,true);
                     moving = 1;
                     lasttime = bot.time.age;
-                    bot.activateItem();
+                    bota.activateItem();
                 }
             }
         }
     });
 
-    bot.on('spawn',function() {
+    bota.on('spawn',function() {
         connected=1;
     });
 
-    bot.on('death',function() {
-        bot.emit("respawn")
+    bota.on('death',function() {
+        bota.emit("respawn")
     });
     
     
